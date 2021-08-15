0afb6b3d6b64ec6a19a12b16c0e256d114212c1e
João Victor <joao.dvlp@gmail.com>
2021-08-15 12:02:37 -0200

    created function to verify if player wins

--- velha.rb
+++ velha.rb
@@ -28,28 +28,38 @@ def verifyIfPositionIsValid(positions, matrix)
 
 end
 
+def verifyIfWin(marker, matrix)
+    if(matrix[0][0] == marker && matrix[0][1] == marker && matrix[0][2] == marker)
+        return marker
+    end
+    return false
+end
+
 def setPositions(positions, matrix, player)
     response = verifyIfPositionIsValid(positions, matrix)
     if(response.class == Array)
         if(player == 1)
             matrix[(response[0].to_i)-1][(response[1].to_i)-1] = 'X'
+            return verifyIfWin('X', matrix)
         else
             matrix[(response[0].to_i)-1][(response[1].to_i)-1] = 'O'
+            return verifyIfWin('O', matrix)
         end
     else
         print response.class
     end
+    
 end
 
 def askForPosition(counter, matrix)
     if(counter % 2 == 0) #par
         print "1 - Write your x,y positions. Like: '1,2': \n"
         positions = gets.chomp
-        setPositions(positions, matrix, 2)
+        return setPositions(positions, matrix, 2)
     else
         print "2 - Write your x positions. Like: '1,2': \n"
         positions = gets.chomp
-        setPositions(positions, matrix, 1)
+        return setPositions(positions, matrix, 1)
     end
 
 end
@@ -59,14 +69,16 @@ def start(matrix)
     rounds = 8
     for i in 0..rounds do
         writeMatrix(matrix)
-        askForPosition(i, matrix)
-        rounds = 1
+        response = askForPosition(i, matrix)
+        if(response != false)
+            break
+        end
     end
 
     print "#########################################\n"
     print "#########################################\n"
-    print "############### G A M E #################\n"
-    print "############### O V E R #################\n"
+    print "############### G 4 M 3 #################\n"
+    print "############### 0 V 3 R #################\n"
     print "#########################################\n"
     print "#########################################\n"
 
