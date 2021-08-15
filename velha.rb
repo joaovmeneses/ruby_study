485324710c6de8d9a629a4fba359cad5ea9a9aff
João Victor <joao.dvlp@gmail.com>
2021-08-15 08:30:45 -0200

    removed unnecessary code

--- velha.rb
+++ velha.rb
@@ -1,11 +1,3 @@
-#config
-rounds = 0
-winner = 0
-#players
-p1 = 'player 1'
-p2 = 'player 2'
-#counter
-counter = 1
 #matrix 3x3
 matrix = [[0,0,0],[0,0,0],[0,0,0]]
 
@@ -20,29 +12,29 @@ def writeMatrix(matrix)
 end
 
 def verifyIfPositionIsValid(positions, matrix)
-
+    
 end
 
 def askForPosition(counter, matrix)
-    if(counter % 2 != 0) #impar
-        puts "1 - Write your x,y positions"
+    if(counter % 2 == 0) #par
+        print "1 - Write your x,y positions. Like: '1,2'"
         positions = gets.chomp.to_i
         verifyIfPositionIsValid(positions, matrix)
     else
-        puts "2 - Write your x positions"
+        print "2 - Write your x positions. Like: '1,2'"
         positions = gets.chomp.to_i
         verifyIfPositionIsValid(positions, matrix)
     end
 
 end
 
-def start(counter, matrix)
-    loop do
-        puts "S T A R T\n"
+def start(matrix)
+    puts "S T A R T\n"
+    for i in 0..8 do
         writeMatrix(matrix)
-        askForPosition(counter, matrix)
+        askForPosition(i, matrix)
         break if winner != 0
     end
 end
 
-start(counter, matrix)
+start(matrix)
