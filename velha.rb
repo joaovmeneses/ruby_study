8d540ac873f309f021e272ab0660ab093884da29
João Victor <joao.dvlp@gmail.com>
2021-08-15 11:33:30 -0200

    start function to verify if position is valid and to setPositions

--- velha.rb
+++ velha.rb
@@ -1,40 +1,71 @@
 #matrix 3x3
-matrix = [[0,0,0],[0,0,0],[0,0,0]]
+matrix = [["#","#","#"],["#","#","#"],["#","#","#"]]
 
 #functions
-
 def writeMatrix(matrix)
+
     text = ''
+    print("  1 2 3\n")
     for i in 0..matrix.length-1 do
-        print matrix[i][0..2].to_s.gsub('[','').gsub(']','').gsub(',','')
+
+        print "#{i+1} #{matrix[i][0..2].to_s.gsub('[','').gsub(']','').gsub(',','').gsub('"', '')}"
+
         print "\n"
     end
+
 end
 
 def verifyIfPositionIsValid(positions, matrix)
-    
+
+    array = positions.split(',')
+    x = array[0]
+    y = array[1]
+    if(array.length > 2 || (x.to_i)-1 > 2 || (y.to_i)-1 > 2)
+        return 0
+    end
+
+    return array
+
+end
+
+def setPositions(positions, matrix)
+    response = verifyIfPositionIsValid(positions, matrix)
+    if(response.class == Array)
+        matrix[(response[0].to_i)-1][(response[1].to_i)-1] = 'X'
+    else
+        print response.class
+    end
 end
 
 def askForPosition(counter, matrix)
     if(counter % 2 == 0) #par
-        print "1 - Write your x,y positions. Like: '1,2'"
-        positions = gets.chomp.to_i
-        verifyIfPositionIsValid(positions, matrix)
+        print "1 - Write your x,y positions. Like: '1,2': \n"
+        positions = gets.chomp
+        setPositions(positions, matrix)
     else
-        print "2 - Write your x positions. Like: '1,2'"
-        positions = gets.chomp.to_i
-        verifyIfPositionIsValid(positions, matrix)
+        print "2 - Write your x positions. Like: '1,2': \n"
+        positions = gets.chomp
+        setPositions(positions, matrix)
     end
 
 end
 
 def start(matrix)
     puts "S T A R T\n"
-    for i in 0..8 do
+    rounds = 8
+    for i in 0..rounds do
         writeMatrix(matrix)
         askForPosition(i, matrix)
-        break if winner != 0
+        rounds = 1
     end
+
+    print "#########################################\n"
+    print "#########################################\n"
+    print "############### G A M E #################\n"
+    print "############### O V E R #################\n"
+    print "#########################################\n"
+    print "#########################################\n"
+
 end
 
 start(matrix)
